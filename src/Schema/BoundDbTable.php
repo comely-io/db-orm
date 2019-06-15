f654d1f62c4eb7f46caae42c734b74f322a6f2e0
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-06-15 12:20:05 +0500

    Bound DB tables

---
+++ src/Schema/BoundDbTable.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * This file is a part of "comely-io/db-orm" package.
+ * https://github.com/comely-io/db-orm
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comely-io/db-orm/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Database\Schema;
+
+use Comely\Database\Database;
+
+/**
+ * Class BoundDbTable
+ * @package Comely\Database\Schema
+ */
+class BoundDbTable implements \Serializable
+{
+    /** @var Database */
+    private $db;
+    /** @var AbstractDbTable */
+    private $table;
+
+    /**
+     * BoundDbTable constructor.
+     * @param Database $db
+     * @param AbstractDbTable $table
+     */
+    public function __construct(Database $db, AbstractDbTable $table)
+    {
+        $this->db = $db;
+        $this->table = $table;
+    }
+
+    /**
+     * @return Database
+     */
+    public function db(): Database
+    {
+        return $this->db;
+    }
+
+    /**
+     * @return AbstractDbTable
+     */
+    public function table(): AbstractDbTable
+    {
+        return $this->table;
+    }
+
+    /**
+     * @return array
+     */
+    public function __debugInfo(): array
+    {
+        return [
+            "db" => sprintf('%s@%s', $this->db->credentials()->host, $this->db->credentials()->name),
+            "table" => $this->table->name
+        ];
+    }
+
+    /**
+     * @return void
+     */
+    public function __clone()
+    {
+        throw new \DomainException('Instance of Schema\BoundDbTable cannot be cloned');
+    }
+
+    /**
+     * @return string|void
+     */
+    public function serialize()
+    {
+        throw new \DomainException('Instance of Schema\BoundDbTable cannot be serialized');
+    }
+
+    /**
+     * @param string $serialized
+     */
+    public function unserialize($serialized)
+    {
+        throw new \DomainException('Instance of Schema\BoundDbTable cannot be serialized');
+    }
+}
