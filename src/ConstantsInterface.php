db8a782847e3005e23ba5fe34d9aa9dd443cc86e
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-07-23 16:15:27 +0500

    migrations bugfix

--- src/ConstantsInterface.php
+++ src/ConstantsInterface.php
@@ -21,7 +21,7 @@ namespace Comely\Database;
 interface ConstantsInterface
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "1.0.14";
+    public const VERSION = "1.0.15";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 10014;
+    public const VERSION_ID = 10015;
 }
