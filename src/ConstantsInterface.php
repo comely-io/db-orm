9f7f8eed991452c211ae990161088bb4ac6355ea
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-09-02 18:27:17 +0500

    version update

--- src/ConstantsInterface.php
+++ src/ConstantsInterface.php
@@ -21,7 +21,7 @@ namespace Comely\Database;
 interface ConstantsInterface
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "1.1.31";
+    public const VERSION = "1.1.32";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 10131;
+    public const VERSION_ID = 10132;
 }
