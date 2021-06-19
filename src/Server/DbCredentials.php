bdf641c267c15ef68d2101ebf7fd063acf378af2
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-06-19 11:56:44 +0500

    database credentials obj

--- src/Server/DbCredentials.php
+++ src/Server/DbCredentials.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * This file is a part of "comely-io/db-orm" package.
  * https://github.com/comely-io/db-orm
  *
@@ -19,77 +19,42 @@ use Comely\Database\Exception\DbConnectionException;
 /**
  * Class DbCredentials
  * @package Comely\Database\Server
- * @property-read string $driver
- * @property-read string $host
- * @property-read null|int $port
- * @property-read null|string $name
- * @property-read null|string $username
- * @property-read null|string $password
- * @property-read bool $persistent
  */
 class DbCredentials
 {
     /** @var string */
-    private $driver;
+    public string $driver;
     /** @var string */
-    private $name;
+    public string $dbname;
     /** @var string */
-    private $host;
+    public string $host;
     /** @var null|int */
-    private $port;
+    public ?int $port = null;
     /** @var null|string */
-    private $username;
+    public ?string $username = null;
     /** @var null|string */
-    private $password;
+    public ?string $password = null;
     /** @var bool */
-    private $persistent;
+    public bool $persistent = false;
 
     /**
      * DbCredentials constructor.
      * @param string $driver
+     * @param string $dbname
+     * @param string $host
+     * @param int|null $port
      * @throws DbConnectionException
      */
-    public function __construct(string $driver)
+    public function __construct(string $driver, string $dbname, string $host = "localhost", ?int $port = null)
     {
         $this->driver = strtolower($driver);
         if (!in_array($this->driver, \PDO::getAvailableDrivers())) {
             throw new DbConnectionException('Invalid database driver or is not supported');
         }
 
-        $this->host = "localhost";
-        $this->persistent = false;
-    }
-
-    /**
-     * @param string $prop
-     * @return mixed
-     */
-    public function __get(string $prop)
-    {
-        switch ($prop) {
-            case "driver":
-            case "name":
-            case "host":
-            case "port":
-            case "username":
-            case "password":
-            case "persistent":
-                return $this->$prop;
-        }
-
-        throw new \OutOfBoundsException('Cannot access inaccessible property');
-    }
-
-    /**
-     * @return array
-     */
-    public function __debugInfo(): array
-    {
-        return [
-            "driver" => $this->driver,
-            "database" => $this->name,
-            "persistent" => $this->persistent
-        ];
+        $this->dbname = $dbname;
+        $this->host = $host ?? "localhost";
+        $this->port = $port;
     }
 
     /**
@@ -98,38 +63,17 @@ class DbCredentials
      */
     public function dsn(): string
     {
-        if (!$this->name) {
+        if (!$this->dbname) {
             throw new DbConnectionException('Cannot get DSN; Database name is not set');
         }
 
         switch ($this->driver) {
             case "sqlite":
-                return sprintf('sqlite:%s', $this->name);
+                return sprintf('sqlite:%s', $this->dbname);
             default:
-                return sprintf('%s:host=%s;dbname=%s;charset=utf8mb4', $this->driver, $this->host, $this->name);
+                $port = $this->port ? "port=" . $this->port . ";" : "";
+                return sprintf('%s:host=%s;%sdbname=%s;charset=utf8mb4', $this->driver, $this->host, $port, $this->dbname);
         }
-    }
-
-    /**
-     * @param string $host
-     * @param int|null $port
-     * @return DbCredentials
-     */
-    public function server(string $host, ?int $port = null): self
-    {
-        $this->host = $host;
-        $this->port = $port;
-        return $this;
-    }
-
-    /**
-     * @param string $name
-     * @return DbCredentials
-     */
-    public function database(string $name): self
-    {
-        $this->name = $name;
-        return $this;
     }
 
     /**
@@ -137,19 +81,10 @@ class DbCredentials
      * @param string|null $password
      * @return DbCredentials
      */
-    public function credentials(string $username, ?string $password = null): self
+    public function login(string $username, ?string $password = null): self
     {
         $this->username = $username;
         $this->password = $password;
-        return $this;
-    }
-
-    /**
-     * @return DbCredentials
-     */
-    public function persistent(): self
-    {
-        $this->persistent = true;
         return $this;
     }
 }
