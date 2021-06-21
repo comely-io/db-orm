4bf73eabb78beee4d186a87f74c2ee830979f193
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-06-21 06:45:09 +0500

    abstract db tables

--- src/Schema/AbstractDbTable.php
+++ src/Schema/AbstractDbTable.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * This file is a part of "comely-io/db-orm" package.
  * https://github.com/comely-io/db-orm
  *
@@ -18,32 +18,33 @@ use Comely\Database\Schema;
 use Comely\Database\Schema\ORM\FindQuery;
 use Comely\Database\Schema\Table\Columns;
 use Comely\Database\Schema\Table\Constraints;
-use Comely\Utils\OOP\OOP;
 
 /**
  * Class AbstractDbTable
  * @package Comely\Database\Schema
- * @method void onConstruct()
  * @property-read null|string $name
  * @property-read null|string $engine
- * @property-read null|string $model
+ * @property-read null|string $ormClass
  */
 abstract class AbstractDbTable
 {
-    public const NAME = null;
-    public const ENGINE = 'InnoDB';
-    public const MODEL = null;
+    /** @var string Table name */
+    public const TABLE = null;
+    /** @var string|null ORM models class */
+    public const ORM_CLASS = null;
+    /** @var string */
+    public const ENGINE = "InnoDB";
 
     /** @var Columns */
-    protected $columns;
+    protected Columns $columns;
     /** @var Constraints */
-    protected $constraints;
+    protected Constraints $constraints;
     /** @var string */
-    protected $name;
+    protected string $name;
     /** @var string */
-    protected $engine;
-    /** @var string */
-    protected $modelsClass;
+    protected string $engine;
+    /** @var string|null */
+    protected ?string $ormClass = null;
 
     /**
      * AbstractDbTable constructor.
@@ -54,46 +55,55 @@ abstract class AbstractDbTable
         $this->constraints = new Constraints();
 
         // Get table names and engine
-        $this->name = static::NAME;
-        if (!is_string($this->name) || !preg_match('/^\w+$/', $this->name)) {
-            throw new \InvalidArgumentException(sprintf('Invalid NAME const for table "%s"', get_called_class()));
+        $this->name = static::TABLE;
+        if (!is_string($this->name) || !$this->name) {
+            throw new \InvalidArgumentException(sprintf('Invalid TABLE const for table "%s"', get_called_class()));
         }
 
         $this->engine = static::ENGINE;
-        if (!is_string($this->engine) || !preg_match('/^\w+$/', $this->engine)) {
+        if (!is_string($this->engine) || !$this->engine) {
             throw new \InvalidArgumentException(sprintf('Invalid ENGINE const for table "%s"', get_called_class()));
         }
 
         // Models class
-        $this->modelsClass = static::MODEL;
-        if (!is_null($this->modelsClass)) {
-            if (!OOP::isValidClass($this->modelsClass)) {
+        $this->ormClass = static::ORM_CLASS;
+        if (!is_null($this->ormClass)) {
+            if (class_exists($this->ormClass)) {
                 throw new \InvalidArgumentException(
-                    sprintf('MODEL const for table "%s" must be a valid class or NULL', get_called_class())
+                    sprintf('defined ORM_CLASS for table "%s" does not exist', get_called_class())
                 );
             }
 
             try {
-                $reflect = new \ReflectionClass($this->modelsClass);
-                $modelIsORM = $reflect->isSubclassOf('Comely\Database\Schema\ORM\Abstract_ORM_Model');
-            } catch (\Exception $e) {
+                $reflect = new \ReflectionClass($this->ormClass);
+                $isValidORMClass = $reflect->isSubclassOf('Comely\Database\Schema\ORM\Abstract_ORM_Model');
+            } catch (\Exception) {
             }
 
-            if (!isset($modelIsORM) || !$modelIsORM) {
+            if (!isset($isValidORMClass) || !$isValidORMClass) {
                 throw new \InvalidArgumentException(
-                    sprintf('MODEL const for table "%s" is not sub class of ORM', get_called_class())
+                    sprintf('define ORM_CLASS for table "%s" is not subclass of ORM', get_called_class())
                 );
             }
         }
 
         // On Construct Callback
-        if (method_exists($this, "onConstruct")) {
-            call_user_func([$this, "onConstruct"]);
-        }
+        $this->onConstruct();
 
         // Callback schema method for table structure
         $this->structure($this->columns, $this->constraints);
     }
+
+    /**
+     * @return void
+     */
+    abstract public function onConstruct(): void;
+
+    /**
+     * @param Columns $cols
+     * @param Constraints $constraints
+     */
+    abstract public function structure(Columns $cols, Constraints $constraints): void;
 
     /**
      * @param string $prop
@@ -105,8 +115,8 @@ abstract class AbstractDbTable
             case "name":
             case "engine":
                 return $this->$prop;
-            case "model":
-                return $this->modelsClass;
+            case "ormClass":
+                return $this->ormClass;
         }
 
         throw new \DomainException('Cannot get value of inaccessible property');
@@ -135,13 +145,7 @@ abstract class AbstractDbTable
      */
     public static function Find(?array $match = null): FindQuery
     {
-        $modelFindQuery = new FindQuery(Schema::Table(strval(static::NAME)));
+        $modelFindQuery = new FindQuery(Schema::Table(strval(static::TABLE)));
         return $match ? $modelFindQuery->match($match) : $modelFindQuery;
     }
-
-    /**
-     * @param Columns $cols
-     * @param Constraints|null $constraints
-     */
-    abstract public function structure(Columns $cols, Constraints $constraints): void;
 }
